c15dc0c95c59479d650f1010395fbd182f36c977
k906506 <k906506@naver.com>
2022-03-03 21:35:26 +0900

    [fix] Add Return value as Parcelable

--- GoogleMap/app/src/main/java/com/example/googlemap/model/SearchResultEntity.kt
+++ GoogleMap/app/src/main/java/com/example/googlemap/model/SearchResultEntity.kt
@@ -1,7 +1,11 @@
 package com.example.googlemap.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class SearchResultEntity(
     val fullAddress: String,
     val name: String,
     val locationLatLng: LocationLatLngEntity
-)
+) : Parcelable
