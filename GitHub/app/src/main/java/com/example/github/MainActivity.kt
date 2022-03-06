0741b90e19dbf068080c163923369a30f9ec45ee
k906506 <k906506@naver.com>
2022-03-06 11:50:39 +0900

    [feat] Add Coroutine function

--- GitHub/app/src/main/java/com/example/github/MainActivity.kt
+++ GitHub/app/src/main/java/com/example/github/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.github
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+import com.example.github.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    val job: Job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+
+    private fun loginGithub() {
+        val loginUrl = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", Key.GITHUB_CLIENT_ID)
+            .build()
+
+        CustomTabsIntent.Builder().build().also {
+            it.launchUrl(this, loginUrl)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        // todo getAccessToken
+        intent?.data?.getQueryParameter("code")?.let {
+            launch(coroutineContext) {
+                getAccessToken(it)
+            }
+        }
+    }
+
+    private suspend fun getAccessToken(code: String) = withContext(Dispatchers.IO) {
     }
 }
