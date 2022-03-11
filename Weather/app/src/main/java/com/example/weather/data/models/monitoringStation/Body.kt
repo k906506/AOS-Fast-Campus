3db71814642973d01cbb870844cb02cfce948f0c
k906506 <k906506@naver.com>
2022-03-11 12:06:19 +0900

    [feat] Add Retrofit model about monitoringStation

---
+++ Weather/app/src/main/java/com/example/weather/data/models/monitoringStation/Body.kt
@@ -0,0 +1,15 @@
+package com.example.weather.data.models.monitoringStation
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Body(
+    @SerializedName("items")
+    val items: List<MonitoringStation>?,
+    @SerializedName("numOfRows")
+    val numOfRows: Int?,
+    @SerializedName("pageNo")
+    val pageNo: Int?,
+    @SerializedName("totalCount")
+    val totalCount: Int?
+)
