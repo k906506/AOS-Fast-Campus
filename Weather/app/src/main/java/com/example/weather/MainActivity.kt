a426484282b5ee581f44827898b64c3afe533005
k906506 <k906506@naver.com>
2022-03-13 19:53:03 +0900

    [feat] Add feature about Widget

--- Weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ Weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -4,39 +4,53 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.view.isGone
+import com.example.weather.data.models.airQuality.Grade
+import com.example.weather.data.models.airQuality.MeasuredValue
+import com.example.weather.data.models.monitoringStation.MonitoringStation
+import com.example.weather.data.util.RetrofitUtil
 import com.example.weather.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.CancellationToken
 import com.google.android.gms.tasks.CancellationTokenSource
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    private val fusedLocationProviderClient by lazy {
-        LocationServices.getFusedLocationProviderClient(this)
-    }
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    private val cancellationTokenSource by lazy {
-        CancellationTokenSource()
-    }
+    private var cancellationTokenSource: CancellationTokenSource? = null
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val scope = MainScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        bindViews()
+        initVariables()
         requestLocationPermissions()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        cancellationTokenSource.cancel()
+        cancellationTokenSource?.cancel()
+        scope.cancel()
     }
 
     private fun requestLocationPermissions() {
@@ -47,16 +61,38 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private fun requestBackgroundLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            backgroundPermissions,
+            REQUEST_ACCESS_BACKGROUND_LOCATION_PERMISSIONS
+        )
+    }
+
+    private fun bindViews() = with(binding) {
+        refresh.setOnRefreshListener {
+            fetchAirQualityData()
+        }
+    }
+
+    private fun initVariables() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
     companion object {
         val permissions = arrayOf(
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
         )
 
+        val backgroundPermissions = arrayOf(
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
+
         const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
+        const val REQUEST_ACCESS_BACKGROUND_LOCATION_PERMISSIONS = 101
     }
 
-    @SuppressLint("MissingPermission")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -66,15 +102,100 @@ class MainActivity : AppCompatActivity() {
         val locationPermissionGranted =
             requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS && grantResults[0] == PackageManager.PERMISSION_GRANTED
 
-        if (!locationPermissionGranted) {
-            finish()
+        val backgroundLocationPermissionGranted =
+            requestCode == REQUEST_ACCESS_BACKGROUND_LOCATION_PERMISSIONS && grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!backgroundLocationPermissionGranted) {
+                requestBackgroundLocationPermissions()
+            } else {
+                fetchAirQualityData()
+            }
         } else {
-            fusedLocationProviderClient.getCurrentLocation(
-                LocationRequest.PRIORITY_HIGH_ACCURACY,
-                cancellationTokenSource.token
-            ).addOnSuccessListener {
-                Toast.makeText(this, it.latitude.toString(), Toast.LENGTH_SHORT).show()
+            if (!locationPermissionGranted) {
+                finish()
+            } else {
+                fetchAirQualityData()
             }
         }
     }
+
+    @SuppressLint("MissingPermission")
+    private fun fetchAirQualityData() {
+        cancellationTokenSource = CancellationTokenSource()
+
+        fusedLocationProviderClient.getCurrentLocation(
+            LocationRequest.PRIORITY_HIGH_ACCURACY,
+            cancellationTokenSource?.token
+        ).addOnSuccessListener {
+            scope.launch {
+                binding.errorDescriptionTextView.isGone = true
+                try {
+                    val monitoringStation =
+                        RetrofitUtil.getNearByMonitoringStation(it.latitude, it.longitude)
+                    val measuredValue =
+                        RetrofitUtil.getLatestAirQualityData(monitoringStation?.stationName!!)
+
+                    displayAirQualityData(monitoringStation, measuredValue!!)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    binding.errorDescriptionTextView.visibility = View.INVISIBLE
+                    binding.contentLayout.alpha = 0F
+                } finally {
+                    binding.progressBar.isGone = true
+                    binding.refresh.isRefreshing = false
+                }
+            }
+        }
+    }
+
+    private fun displayAirQualityData(
+        monitoringStation: MonitoringStation,
+        measuredValue: MeasuredValue
+    ) =
+        with(binding) {
+            contentLayout.animate()
+                .alpha(1F)
+                .start()
+
+            measuringStationNameTextView.text = monitoringStation.stationName
+            measuringStationAddressTextView.text = monitoringStation.addr
+
+            (measuredValue.khaiGrade ?: Grade.UNKNOWN).let {
+                root.setBackgroundResource(it.colorResId)
+                totalGradeLabelTextView.text = it.label
+                totalGradeEmojiTextView.text = it.emoji
+            }
+
+            with(measuredValue) {
+                fineDustInformationTextView.text =
+                    "미세먼지: ${pm10Value} ㎍/㎥ ${(pm10Grade ?: Grade.UNKNOWN).emoji}"
+                ultraFineDustInformationTextView.text =
+                    "초미세먼지: ${pm25Value} ㎍/㎥ ${(pm25Grade ?: Grade.UNKNOWN).emoji}"
+
+                with(so2Item) {
+                    labelTextView.text = "아황산가스"
+                    gradeTextView.text = (so2Grade ?: Grade.UNKNOWN).toString()
+                    valueTextView.text = "$so2Value ppm"
+                }
+
+                with(coItem) {
+                    labelTextView.text = "일산화탄소"
+                    gradeTextView.text = (coGrade ?: Grade.UNKNOWN).toString()
+                    valueTextView.text = "$coValue ppm"
+                }
+
+                with(o3Item) {
+                    labelTextView.text = "오존"
+                    gradeTextView.text = (o3Grade ?: Grade.UNKNOWN).toString()
+                    valueTextView.text = "$o3Value ppm"
+                }
+
+                with(no2Item) {
+                    labelTextView.text = "이산화질소"
+                    gradeTextView.text = (no2Grade ?: Grade.UNKNOWN).toString()
+                    valueTextView.text = "$no2Value ppm"
+                }
+            }
+        }
 }
