996588521b5aaa0513b2ccfd181532c33f372798
k906506 <k906506@naver.com>
2022-03-15 21:29:19 +0900

    [Feat] 사진 관련 모델 추가

---
+++ ImageViewer/app/src/main/java/com/example/imageviewer/data/models/Position.kt
@@ -0,0 +1,11 @@
+package com.example.imageviewer.data.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Position(
+    @SerializedName("latitude")
+    val latitude: Double?,
+    @SerializedName("longitude")
+    val longitude: Double?
+)
