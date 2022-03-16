fd37b1dea7de6020d54a4a2ee9ae1663deaba1fa
k906506 <k906506@naver.com>
2022-03-16 11:29:09 +0900

    [feat] Search, Refresh 기능 구현

--- ImageViewer/app/src/main/java/com/example/imageviewer/MainActivity.kt
+++ ImageViewer/app/src/main/java/com/example/imageviewer/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.imageviewer
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.imageviewer.data.util.RetrofitUtil
+import com.example.imageviewer.databinding.ActivityMainBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val adapter by lazy {
+        PhotoAdapter()
+    }
+
+    private val scope = MainScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initViews()
+        bindViews()
+        fetchRandomPhotos()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+    }
+
+    private fun fetchRandomPhotos(query: String? = null) = scope.launch {
+        RetrofitUtil.getRandomPhotos(query)?.let { photos ->
+            adapter.submitList(photos)
+        }
+
+        binding.refreshLayout.isRefreshing = false
+    }
+
+    private fun initViews() = with(binding) {
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.adapter = adapter
+    }
+
+    private fun bindViews() = with(binding) {
+        searchEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                currentFocus?.let { view ->
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                    inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
+
+                    view.clearFocus()
+
+
+                    fetchRandomPhotos(searchEditText.text.toString())
+                }
+            }
+            true
+        }
+
+        refreshLayout.setOnRefreshListener {
+            fetchRandomPhotos(binding.searchEditText.text.toString())
+        }
     }
 }
