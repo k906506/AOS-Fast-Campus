344cf07a4a07cc37db3d0d4ed4fb85cd01c2259b
k906506 <k906506@naver.com>
2022-03-17 22:22:38 +0900

    [feat] Flash 기능 추가

--- CameraX/app/src/main/java/com/example/camerax/MainActivity.kt
+++ CameraX/app/src/main/java/com/example/camerax/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.example.camerax
 
 import android.Manifest
-import android.content.Context
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.pm.PackageManager
-import android.hardware.display.DisplayManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.MediaStore
+import android.view.ScaleGestureDetector
 import androidx.camera.core.*
 import androidx.camera.core.ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY
 import androidx.camera.core.ImageCapture.FLASH_MODE_AUTO
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isGone
+import com.bumptech.glide.Glide
 import com.example.camerax.databinding.ActivityMainBinding
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    private var displayId: Int = -1
     private var camera: Camera? = null
+    private var _isCaptured: Boolean = false
+    private var _flashOn: Boolean = false
 
-    private lateinit var cameraExecutor: ExecutorService
     private lateinit var imageCapture: ImageCapture
 
     private val binding by lazy {
@@ -32,24 +37,8 @@ class MainActivity : AppCompatActivity() {
         ProcessCameraProvider.getInstance(this)
     }
 
-    private val displayManager by lazy {
-        getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
-    }
-
     private val cameraMainExecutor by lazy {
         ContextCompat.getMainExecutor(this)
-    }
-
-    private val displayListener = object : DisplayManager.DisplayListener {
-        override fun onDisplayAdded(displayId: Int) = Unit
-
-        override fun onDisplayRemoved(displayId: Int) = Unit
-
-        override fun onDisplayChanged(displayId: Int) {
-            if (this@MainActivity.displayId == displayId) {
-
-            }
-        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,21 +46,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         requestPermissions()
-
-    }
-
-
-    private fun startCamera(viewFinder: PreviewView) {
-        displayManager.registerDisplayListener(displayListener, null)
-        cameraExecutor = Executors.newSingleThreadExecutor()
-        viewFinder.postDelayed({
-            displayId = viewFinder.display.displayId
-            bindCameraUseCase()
-        }, 10)
+        bindCaptureListener()
+        bindZoomListener()
+        bindFlashListener()
     }
 
     private fun bindCameraUseCase() = with(binding) {
-        val rotation = viewFinder.display.rotation
         val cameraSelector = CameraSelector.Builder()
             .requireLensFacing(LENS_FACING)
             .build()
@@ -81,13 +61,11 @@ class MainActivity : AppCompatActivity() {
                 val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
                 val preview = Preview.Builder().apply {
                     setTargetAspectRatio(AspectRatio.RATIO_16_9)
-                    setTargetRotation(rotation)
                 }.build()
 
                 val imageCaptureBuilder = ImageCapture.Builder()
                     .setCaptureMode(CAPTURE_MODE_MINIMIZE_LATENCY)
                     .setTargetAspectRatio(AspectRatio.RATIO_16_9)
-                    .setTargetRotation(rotation)
                     .setFlashMode(FLASH_MODE_AUTO)
 
                 imageCapture = imageCaptureBuilder.build()
@@ -98,12 +76,107 @@ class MainActivity : AppCompatActivity() {
                         this@MainActivity, cameraSelector, preview, imageCapture
                     )
                     preview.setSurfaceProvider(viewFinder.surfaceProvider)
-
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
             }, cameraMainExecutor
         )
+    }
+
+    private fun bindCaptureListener() = with(binding) {
+        captureButton.setOnClickListener {
+            if (_isCaptured.not()) {
+                _isCaptured = true
+                captureCamera()
+            }
+        }
+    }
+
+    private var contentUri: Uri? = null
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun bindZoomListener() = with(binding) {
+
+        val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                val currentZoomRatio = camera?.cameraInfo?.zoomState?.value?.zoomRatio ?: 1f
+                val delta = detector.scaleFactor
+
+                camera?.cameraControl?.setZoomRatio(currentZoomRatio * delta)
+                return true
+            }
+        }
+
+        val scaleGestureDetector = ScaleGestureDetector(this@MainActivity, listener)
+        viewFinder.setOnTouchListener { v, event ->
+            scaleGestureDetector.onTouchEvent(event)
+            return@setOnTouchListener true
+        }
+    }
+
+    private fun captureCamera() {
+        if (::imageCapture.isInitialized.not()) return
+
+        val fileName = "${System.currentTimeMillis()}.jpg"
+
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            }
+        }
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            imageDetails
+        ).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            cameraMainExecutor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    contentUri = outputFileResults.savedUri
+                    bindThumbnailImageView(contentUri)
+                    _isCaptured = false
+                }
+
+                override fun onError(e: ImageCaptureException) {
+                    e.printStackTrace()
+                    _isCaptured = false
+                }
+            })
+    }
+
+    private fun bindThumbnailImageView(contentUri: Uri?) = with(binding) {
+        Handler(Looper.getMainLooper()).post {
+            Glide.with(root)
+                .load(contentUri)
+                .centerCrop()
+                .into(thumbnailImageView)
+        }
+    }
+
+    private fun bindFlashListener() = with(binding) {
+        val hasFlash = camera?.cameraInfo?.hasFlashUnit() ?: false
+        if (hasFlash) {
+            flashButton.setOnClickListener {
+                changeFlashState()
+            }
+        } else {
+            flashButton.isGone = true
+        }
+    }
+
+    private fun changeFlashState() {
+        if (_flashOn) { // 켜진 상태 -> 꺼진 상태로 변경
+            camera?.cameraControl?.enableTorch(_flashOn.not())
+            _flashOn = _flashOn.not()
+        } else { // 꺼진 상태 -> 켜진 상태로 변경
+            camera?.cameraControl?.enableTorch(_flashOn.not())
+            _flashOn = _flashOn.not()
+        }
     }
 
     private fun requestPermissions() {
@@ -122,14 +195,15 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if (requestCode == REQUEST_CAMERA_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            startCamera(binding.viewFinder)
+            bindCameraUseCase()
         }
     }
 
     companion object {
         private const val REQUEST_CAMERA_PERMISSION = 100
         private val permissions = arrayOf(
-            Manifest.permission.CAMERA
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
         private val LENS_FACING = CameraSelector.LENS_FACING_BACK
     }
