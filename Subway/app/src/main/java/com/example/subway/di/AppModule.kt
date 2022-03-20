ff070389336c9fa7c1bbd79c6894c0ef83653c76
k906506 <k906506@naver.com>
2022-03-20 17:17:19 +0900

    [feat] AppModel 추가

--- Subway/app/src/main/java/com/example/subway/di/AppModule.kt
+++ Subway/app/src/main/java/com/example/subway/di/AppModule.kt
@@ -1,5 +1,34 @@
 package com.example.subway.di
 
+import android.app.Activity
+import com.example.subway.data.api.StationApi
+import com.example.subway.data.api.StationStorageApi
+import com.example.subway.data.db.AppDatabase
+import com.example.subway.data.preference.PreferenceManager
+import com.example.subway.data.preference.SharedPreferenceManager
+import com.example.subway.data.repository.StationRepository
+import com.example.subway.data.repository.StationRepositoryImpl
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
-val appModule = module { }
+val appModule = module {
+    single { Dispatchers.IO }
+
+    // Database
+    single { AppDatabase.build(androidApplication()) }
+    single { get<AppDatabase>().stationDao() }
+
+    // Preference
+    single { androidContext().getSharedPreferences("preference", Activity.MODE_PRIVATE) }
+    single<PreferenceManager> { SharedPreferenceManager(get()) }
+
+    // Api
+    single<StationApi> { StationStorageApi(Firebase.storage) }
+
+    // Repository
+    single<StationRepository> { StationRepositoryImpl(get(), get(), get(), get()) }
+}
