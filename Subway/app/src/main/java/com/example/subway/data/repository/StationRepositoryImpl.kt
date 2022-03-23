ede240f1c2fbaac553991279b286dbbba7ee6313
k906506 <k906506@naver.com>
2022-03-23 12:57:59 +0900

    [feat] Station 관련 Repository 수정

--- Subway/app/src/main/java/com/example/subway/data/repository/StationRepositoryImpl.kt
+++ Subway/app/src/main/java/com/example/subway/data/repository/StationRepositoryImpl.kt
@@ -31,17 +31,7 @@ class StationRepositoryImpl(
         val lastDatabaseUpdatedTimeMillis = preferenceManager.getLong(KEY_LAST_DATABASE_UPDATED_TIME_MILLIS)
 
         if (lastDatabaseUpdatedTimeMillis == null || fileUpdatedTimeMillis > lastDatabaseUpdatedTimeMillis) {
-            val stationSubways = stationApi.getStationSubways()
-            stationDao.insertStations(stationSubways.map { it.first })
-            stationDao.insertSubways(stationSubways.map { it.second })
-            stationDao.insertCrossReferences(
-                stationSubways.map { (station, subway) ->
-                    StationSubwayCrossRefEntity(
-                        station.stationName,
-                        subway.subwayId
-                    )
-                }
-            )
+            stationDao.insertStationSubways(stationApi.getStationSubways())
             preferenceManager.putLong(KEY_LAST_DATABASE_UPDATED_TIME_MILLIS, fileUpdatedTimeMillis)
         }
     }
